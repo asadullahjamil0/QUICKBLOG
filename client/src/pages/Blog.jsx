ed0b4a68add3dd4472920b918a9be1162e2083cc
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-08-27 17:49:01 +0500

    Working on blog page for different blogs.

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { blog_data } from '../assets/assets';
 
 function Blog() {
-    return (
+    const { id } = useParams();
+    const [data, setData] = useState(null);
+
+    const fetchBlogData = async () => {
+        const data = blog_data.find(item => item._id === id)
+        setData(data)
+    }
+
+    useEffect(() => {
+        fetchBlogData();
+    }, [])
+
+    return data ? (
         <div>
             <h1>Blog</h1>
         </div>
-    )
+    ) : <div>Loading...</div>
 }
 
 export default Blog
